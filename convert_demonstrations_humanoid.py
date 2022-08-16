83d0794405b4a3eb163757220c3d18e83507b5c7
chenu <chenu@Alex-ISIR.local>
2022-08-16 16:18:39 +0200

    fix dimension in demo

--- convert_demonstrations_humanoid.py
+++ convert_demonstrations_humanoid.py
@@ -5,22 +5,24 @@ import numpy as np
 L_demo = []
 for i in range(1,2):
 
-    with open('./demonstrations/humanoidenv/' + str(i) + '.demo', 'rb') as handle:
-        demo = pickle.load(handle)
+	with open('./demonstrations/humanoidenv/' + str(i) + '.demo', 'rb') as handle:
+		demo = pickle.load(handle)
 
-    print("demo.keys() = ", demo.keys())
-    new_demo = []
-    for obs, action in zip(demo["observations"],demo["actions"]):
-        print("obs = ", obs)
-        transition = {"observation":np.array(obs).astype(np.float32), "action":action.astype(np.float32), "reward":np.array(0.,dtype=np.float32)}
-        print("transition = ", transition)
-        new_demo.append(transition)
+	print("demo.keys() = ", demo.keys())
+	new_demo = []
+	for obs, action in zip(demo["observations"],demo["actions"]):
+		print("obs shape = ", np.array(obs).shape)
+		print("action shape = ", np.array(action).reshape(-1,).shape)
+
+		transition = {"observation":np.array(obs).astype(np.float32), "action":action.reshape(-1,).astype(np.float32), "reward":np.array(0.,dtype=np.float32)}
+		# print("transition = ", transition)
+		new_demo.append(transition)
 
 
-    L_demo.append(new_demo)
+	L_demo.append(new_demo)
 
 print(len(L_demo))
 
 
 with open('./demonstrations/humanoidenv/HumanoidEnv-v0.pkl', 'wb') as handle:
-    pickle.dump(L_demo, handle, protocol=4)
+	pickle.dump(L_demo, handle, protocol=4)
