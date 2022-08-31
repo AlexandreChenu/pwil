38d32e0714f695f365d30518e5ad9f8d6ad79935
chenu <chenu@Alex-ISIR.local>
2022-08-31 15:34:58 +0200

    add demonstration for humanoid standup

---
+++ convert_demonstrations_humanoid_standup.py
@@ -0,0 +1,29 @@
+import pickle5 as pickle
+import numpy as np
+
+
+L_demo = []
+for i in range(1,2):
+
+	with open('./demonstrations/humanoid_standup_env/' + str(i) + '.demo', 'rb') as handle:
+		demo = pickle.load(handle)
+
+	print("demo.keys() = ", demo.keys())
+	new_demo = []
+	for obs, action in zip(demo["observations"],demo["actions"]):
+		print("obs shape = ", np.array(obs).shape)
+		print("action shape = ", np.array(action).reshape(-1,).shape)
+
+		transition = {"observation":np.array(obs).astype(np.float32), "action":action.reshape(-1,).astype(np.float32), "reward":np.array(0.,dtype=np.float32)}
+		# print("transition = ", transition)
+		new_demo.append(transition)
+	print("len(new_demo) = ", len(new_demo))
+
+	L_demo.append(new_demo)
+	print("len(L_demo) = ", len(L_demo))
+
+print(len(L_demo))
+
+
+with open('./demonstrations/humanoid_standup_env/HumanoidStandupEnv-v0.pkl', 'wb') as handle:
+	pickle.dump(L_demo, handle, protocol=4)
